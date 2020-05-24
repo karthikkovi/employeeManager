04cf210b78d8ad7b4ff23d78dce7a7d8c4412928
Karthik Kovi <karthi.kovi@gmail.com>
2020-05-24 13:12:48 +0930

    updated basic prompts

--- server.js
+++ server.js
@@ -23,5 +23,50 @@ connection.connect(function (error) {
 });
 
 async function init() {
-    // 
+    // Intial prompt
+
+    function userPrompt() {
+        return inquirer.prompt({
+            name: "viewAddUpdate",
+            type: "list",
+            message: "Would you like Add, view or update a new employees or deparments?",
+            choices: ["Add", "View", "Update", "Exit"]
+        })
+    }
+
+    let userRequirement = await userPrompt();
+
+    switch (userRequirement.viewAddUpdate) {
+        case "Add":
+            addsomething();
+            break;
+        case "View":
+            viewSomething();
+            break;
+        case "Update":
+            updateSomething();
+            break;
+        default:
+            console.log("Have a great day!")
+            connection.end();
+            process.exit();
+            break;
+    };
 }
+
+async function addsomething(){
+    console.log("something will be added");
+    init()
+} 
+
+async function viewSomething(){
+    console.log("something will be Viewed");
+    init()
+}
+
+async function updateSomething(){
+    console.log("something will be Updated");
+    init()
+}
+
+init();
