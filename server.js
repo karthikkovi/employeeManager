5a6d3f0b5a9bf19b0e9dcfec7b0ec7e30e3bec15
Karthik Kovi <karthi.kovi@gmail.com>
2020-05-24 13:54:36 +0930

    Added code for adding and viewing details

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const table = require("console.table");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "#",
     database: "employeemanager"
 });
 
@@ -54,17 +54,121 @@ async function init() {
     };
 }
 
-async function addsomething(){
-    console.log("something will be added");
-    init()
-} 
+async function addsomething() {
+    function userPrompt() {
+        return inquirer.prompt({
+            name: "viewAddUpdate",
+            type: "list",
+            message: "What would you like to add",
+            choices: ["Employee", "Department", "Roles", "Exit"]
+        })
+    }
 
-async function viewSomething(){
-    console.log("something will be Viewed");
+    let userRequirement = await userPrompt();
+
+    switch (userRequirement.viewAddUpdate) {
+
+        case "Employee":
+            async function newEmployee() {
+                return inquirer.prompt([{
+                    name: "firstName",
+                    message: "Enter the first name: "
+                },
+                {
+                    name: "lastName",
+                    message: "Enter the last name: "
+                },
+                {
+                    name: "roleID",
+                    message: "Enter the role ID: "
+                },
+                {
+                    name: "managerID",
+                    message: "Enter the manager ID, if none exists enter null: "
+                }])
+            }
+
+            let newEmployeeDetails = await newEmployee();
+            connection.query(`INSERT INTO employee(firstName, lastName, roleID, managerID) VALUES ("${newEmployeeDetails.firstName}", "${newEmployeeDetails.lastName}", "${newEmployeeDetails.roleID}", "${newEmployeeDetails.managerID}")`)
+
+            console.table(await connection.query("SELECT * FROM employee"));
+            break;
+
+        case "Department":
+            async function newDepartment() {
+                return inquirer.prompt({
+                    name: "departmentName",
+                    message: "Enter the department name: "
+                })
+            }
+
+            let newDepartmentDetails = await newDepartment();
+            connection.query(`INSERT INTO department(departmentName) VALUES ("${newDepartmentDetails.departmentName}")`)
+
+            console.table(await connection.query("SELECT * FROM department"));
+            break;
+
+        case "Roles":
+            case "Employee":
+            async function newRole() {
+                return inquirer.prompt([{
+                    name: "designation",
+                    message: "Enter the designation: "
+                },
+                {
+                    name: "salary",
+                    message: "Enter the salary: "
+                },
+                {
+                    name: "departmentID",
+                    message: "Enter the department ID: "
+                }])
+            }
+
+            let newRoleDetails = await newRole();
+            connection.query(`INSERT INTO employeeRole(designation, salary, departmentID) VALUES ("${newRoleDetails.designation}", "${newRoleDetails.salary}", "${newRoleDetails.departmentID}")`)
+
+            console.table(await connection.query("SELECT * FROM employeeRole"));
+            break;
+
+        default:
+            break;
+    }
+
+    init();
+}
+
+async function viewSomething() {
+    function userPrompt() {
+        return inquirer.prompt({
+            name: "viewAddUpdate",
+            type: "list",
+            message: "What would you like to view",
+            choices: ["Employee", "Department", "Roles", "Exit"]
+        })
+    }
+
+    let userView = await userPrompt();
+    switch (userView.viewAddUpdate) {
+        case "Employee":
+            console.table(await connection.query("SELECT * FROM employee"));
+            break;
+        
+        case "Department":
+            console.table(await connection.query("SELECT * FROM department"));
+            break;
+        
+        case "Roles":
+            console.table(await connection.query("SELECT * FROM employeeRole"));
+            break;
+        
+        default:
+            break;
+    }
     init()
 }
 
-async function updateSomething(){
+async function updateSomething() {
     console.log("something will be Updated");
     init()
 }
