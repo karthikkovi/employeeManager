4309e13e97cdb2bcbaceb3a933eb99f71f4428b6
Karthik Kovi <karthi.kovi@gmail.com>
2020-05-24 12:20:17 +0930

    Added DB schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employeemanager;
+
+CREATE DATABASE employeemanager;
+
+USE employeemanager;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    employeeName VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employeeRole (
+    id INT NOT NULL AUTO_INCREMENT,
+    designation VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    departmentID INT,
+    FOREIGN KEY (departmentID) REFERENCES deparment(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    roleID INT,
+    managerID INT,
+    FOREIGN KEY (roleID) REFERENCES employeeRole(id),
+    FOREIGN KEY (managerID) REFERENCES employee(id),
+    PRIMARY KEY (id)
+);
